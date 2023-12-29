e91b2ca1e264881a96d687b7dd2afee62daaf044
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-12-29 14:30:12 +0100

    :recycle: Refactor lexer test

--- lexer_test.go
+++ lexer_test.go
@@ -4,98 +4,122 @@ import (
 	"testing"
 )
 
+type lexerTest struct {
+	input          string
+	expectedResult []Token
+}
+
 func TestNextToken(t *testing.T) {
-	inputs := []string{
-		"1 + 2 + 3",
-		"1 * 2 + 3",
-		"1 - 2 * 3",
-		"1 - 2 / 3",
-		"1 * 2 / 3",
-		"1 * 2 * 3",
-		"1 / 2 / 3",
-		"1 - 2 - 3",
-		"1.2 - 2.34 - 3.567",
-		"1 * 2.34 / 3.567",
+	tests := []lexerTest{
+		{
+			input: "1 + 2 + 3",
+			expectedResult: []Token{
+				{Type: NUMBER, Literal: "1"},
+				{Type: PLUS, Literal: "+"},
+				{Type: NUMBER, Literal: "2"},
+				{Type: PLUS, Literal: "+"},
+				{Type: NUMBER, Literal: "3"},
+			},
+		},
+		{
+			input: "1 * 2 + 3",
+			expectedResult: []Token{
+				{Type: NUMBER, Literal: "1"},
+				{Type: MULT, Literal: "*"},
+				{Type: NUMBER, Literal: "2"},
+				{Type: PLUS, Literal: "+"},
+				{Type: NUMBER, Literal: "3"},
+			},
+		},
+		{
+			input: "1 - 2 * 3",
+			expectedResult: []Token{
+				{Type: NUMBER, Literal: "1"},
+				{Type: MINUS, Literal: "-"},
+				{Type: NUMBER, Literal: "2"},
+				{Type: MULT, Literal: "*"},
+				{Type: NUMBER, Literal: "3"},
+			},
+		},
+		{
+			input: "1 - 2 / 3",
+			expectedResult: []Token{
+				{Type: NUMBER, Literal: "1"},
+				{Type: MINUS, Literal: "-"},
+				{Type: NUMBER, Literal: "2"},
+				{Type: DIVIDE, Literal: "/"},
+				{Type: NUMBER, Literal: "3"},
+			},
+		},
+		{
+			input: "1 * 2 / 3",
+			expectedResult: []Token{
+				{Type: NUMBER, Literal: "1"},
+				{Type: MULT, Literal: "*"},
+				{Type: NUMBER, Literal: "2"},
+				{Type: DIVIDE, Literal: "/"},
+				{Type: NUMBER, Literal: "3"},
+			},
+		},
+		{
+			input: "1 * 2 * 3",
+			expectedResult: []Token{
+				{Type: NUMBER, Literal: "1"},
+				{Type: MULT, Literal: "*"},
+				{Type: NUMBER, Literal: "2"},
+				{Type: MULT, Literal: "*"},
+				{Type: NUMBER, Literal: "3"},
+			},
+		},
+		{
+			input: "1 / 2 / 3",
+			expectedResult: []Token{
+				{Type: NUMBER, Literal: "1"},
+				{Type: DIVIDE, Literal: "/"},
+				{Type: NUMBER, Literal: "2"},
+				{Type: DIVIDE, Literal: "/"},
+				{Type: NUMBER, Literal: "3"},
+			},
+		},
+		{
+			input: "1 - 2 - 3",
+			expectedResult: []Token{
+				{Type: NUMBER, Literal: "1"},
+				{Type: MINUS, Literal: "-"},
+				{Type: NUMBER, Literal: "2"},
+				{Type: MINUS, Literal: "-"},
+				{Type: NUMBER, Literal: "3"},
+			},
+		},
+		{
+			input: "1.2 - 2.34 - 3.567",
+			expectedResult: []Token{
+				{Type: NUMBER, Literal: "1.2"},
+				{Type: MINUS, Literal: "-"},
+				{Type: NUMBER, Literal: "2.34"},
+				{Type: MINUS, Literal: "-"},
+				{Type: NUMBER, Literal: "3.567"},
+			},
+		},
+		{
+			input: "1 * 2.34 / 3.567",
+			expectedResult: []Token{
+				{Type: NUMBER, Literal: "1"},
+				{Type: MULT, Literal: "*"},
+				{Type: NUMBER, Literal: "2.34"},
+				{Type: DIVIDE, Literal: "/"},
+				{Type: NUMBER, Literal: "3.567"},
+			},
+		},
 	}
 
-	results := map[int][]Token{
-		0: {
-			{Type: NUMBER, Literal: "1"},
-			{Type: PLUS, Literal: "+"},
-			{Type: NUMBER, Literal: "2"},
-			{Type: PLUS, Literal: "+"},
-			{Type: NUMBER, Literal: "3"},
-		},
-		1: {
-			{Type: NUMBER, Literal: "1"},
-			{Type: MULT, Literal: "*"},
-			{Type: NUMBER, Literal: "2"},
-			{Type: PLUS, Literal: "+"},
-			{Type: NUMBER, Literal: "3"},
-		},
-		2: {
-			{Type: NUMBER, Literal: "1"},
-			{Type: MINUS, Literal: "-"},
-			{Type: NUMBER, Literal: "2"},
-			{Type: MULT, Literal: "*"},
-			{Type: NUMBER, Literal: "3"},
-		},
-		3: {
-			{Type: NUMBER, Literal: "1"},
-			{Type: MINUS, Literal: "-"},
-			{Type: NUMBER, Literal: "2"},
-			{Type: DIVIDE, Literal: "/"},
-			{Type: NUMBER, Literal: "3"},
-		},
-		4: {
-			{Type: NUMBER, Literal: "1"},
-			{Type: MULT, Literal: "*"},
-			{Type: NUMBER, Literal: "2"},
-			{Type: DIVIDE, Literal: "/"},
-			{Type: NUMBER, Literal: "3"},
-		},
-		5: {
-			{Type: NUMBER, Literal: "1"},
-			{Type: MULT, Literal: "*"},
-			{Type: NUMBER, Literal: "2"},
-			{Type: MULT, Literal: "*"},
-			{Type: NUMBER, Literal: "3"},
-		},
-		6: {
-			{Type: NUMBER, Literal: "1"},
-			{Type: DIVIDE, Literal: "/"},
-			{Type: NUMBER, Literal: "2"},
-			{Type: DIVIDE, Literal: "/"},
-			{Type: NUMBER, Literal: "3"},
-		},
-		7: {
-			{Type: NUMBER, Literal: "1"},
-			{Type: MINUS, Literal: "-"},
-			{Type: NUMBER, Literal: "2"},
-			{Type: MINUS, Literal: "-"},
-			{Type: NUMBER, Literal: "3"},
-		},
-		8: {
-			{Type: NUMBER, Literal: "1.2"},
-			{Type: MINUS, Literal: "-"},
-			{Type: NUMBER, Literal: "2.34"},
-			{Type: MINUS, Literal: "-"},
-			{Type: NUMBER, Literal: "3.567"},
-		},
-		9: {
-			{Type: NUMBER, Literal: "1"},
-			{Type: MULT, Literal: "*"},
-			{Type: NUMBER, Literal: "2.34"},
-			{Type: DIVIDE, Literal: "/"},
-			{Type: NUMBER, Literal: "3.567"},
-		},
-	}
+	for i, test := range tests {
+		lexer := NewLexer(test.input)
+		expectedResult := test.expectedResult
 
-	for i, input := range inputs {
-		lexer := NewLexer(input)
-		result := results[i]
-		for _, tt := range result {
+		for _, tt := range expectedResult {
 			tok := lexer.NextToken()
+
 			if tok.Type != tt.Type {
 				t.Fatalf("test [%d] token type wrong. expected=%q, got=%q", i, tt.Type, tok.Type)
 			}
